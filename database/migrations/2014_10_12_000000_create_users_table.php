2b0861a634858bf96035f7a74bb2f915e48c4a94
Ekode <ekode@protonmail.com>
2020-02-26 13:21:25 +0100

    Added User migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('name',100);
+            $table->string('first_name',100);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->date('birth_date');
+            $table->string('address',100);
+            $table->string('phone_number',100)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
