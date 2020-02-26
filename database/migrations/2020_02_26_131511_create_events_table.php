353074a4767e8ec1c2cf754951c10c4ecf2bb972
Ekode <ekode@protonmail.com>
2020-02-26 14:20:19 +0100

    Added events migration

---
+++ database/migrations/2020_02_26_131511_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('date');
+            $table->string('name');
+            $table->text('description');
+            $table->timestamps();
+
+            $table->unsignedBigInteger('id_message');
+            $table->foreign('id_message')->references('id')->on('messages');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events',function(Blueprint $table){
+            $table->dropForeign('id_message');
+        });
+        Schema::dropIfExists('events');
+    }
+}
