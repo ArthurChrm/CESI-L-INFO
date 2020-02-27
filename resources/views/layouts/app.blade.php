4d8e2e5737a5d6d836b33fdd4653b5bfc1e6bf72
Ekode <ekode@protonmail.com>
2020-02-27 09:44:59 +0100

    Added basic double navigation drawers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,8 +20,57 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        @yield('body');
-    </div>
+<div id="app">
+    <!-- Navigation drawers -->
+    <v-app>
+        <v-navigation-drawer app
+            permanent
+        >
+            <v-row class="fill-height" no-gutters>
+                <v-navigation-drawer
+                    dark
+                    mini-variant
+                    mini-variant-width="56"
+                    permanent
+                >
+                    <v-list-item class="px-2">
+                        <v-list-item-avatar>
+                            <v-img src="https://randomuser.me/api/portraits/women/75.jpg"></v-img>
+                        </v-list-item-avatar>
+                    </v-list-item>
+
+                    <v-divider></v-divider>
+
+                    <v-list
+                        dense
+                        nav
+                    >
+
+                        <v-list-item link>
+                            <v-list-item-action>
+                                <v-icon>fas fa-ad</v-icon>
+                            </v-list-item-action>
+
+                            <v-list-item-content>
+                                <v-list-item-title>Test</v-list-item-title>
+                            </v-list-item-content>
+                        </v-list-item>
+                    </v-list>
+                </v-navigation-drawer>
+
+                <v-list class="grow">
+                    <v-list-item link>
+                        <v-list-item-title>Test</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-row>
+        </v-navigation-drawer>
+        <v-content>
+            <v-container fluid>
+                @yield('content')
+            </v-container>
+        </v-content>
+    </v-app>
+</div>
 </body>
 </html>
