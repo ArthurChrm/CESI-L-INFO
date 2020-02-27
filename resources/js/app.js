fb52bab9c1b774488d84a2458446733a550c1eb0
Ekode <ekode@protonmail.com>
2020-02-27 09:00:50 +0100

    Added vue and vuetify for front

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
+import Vuetify from 'vuetify';
+
 require('./bootstrap');
+
+window.Vue = require('vue');
+Vue.use(Vuetify);
+
+const app = new Vue({
+    el: '#app',
+    vuetify: new Vuetify()
+});
