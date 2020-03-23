f8c3d9fd8b976349fd4b02c8015bb396f1bed41c
Ekode <ekode@protonmail.com>
2020-03-23 16:50:25 +0100

    Added basic sync with backend

--- resources/js/components/MessagerieComponent.vue
+++ resources/js/components/MessagerieComponent.vue
@@ -1,27 +1,69 @@
 <template>
-    <div>
-        <v-card
-            height="90%"
-            outlined
-            width="100%"
-        >
-            Test
+    <v-card
+        height="100%"
+        outlined
+        width="100%">
+
+        <v-card id="chatfield" class="pl-10 pt-10 pr-10"
+            v-for="msg in messages"
+            :key="msg.id">
+            <!-- Message entrant -->
+            <v-row align="center" class="mb-3">
+                <v-col class="col-auto">
+                    <v-avatar color="">
+                        <v-icon>fa-user</v-icon>
+                    </v-avatar>
+                </v-col>
+
+                <v-card outlined>
+                    <v-card-text>{{ msg.text }}</v-card-text>
+                    <v-divider></v-divider>
+                    <v-card-actions class="pl-5 pr-5 pt-5 pb-5" v-if="Object.keys(msg.files).length !== 0 || Object.keys(msg.events).length !== 0">
+                        <v-spacer></v-spacer>
+                        <v-badge content="2" overlap color="red" class="mr-3" v-if="Object.keys(msg.files).length !== 0">
+                            <v-btn icon>
+                                <v-icon>fa-file-alt</v-icon>
+                            </v-btn>
+                        </v-badge>
+                        <v-badge content="1" overlap color="red" v-if="Object.keys(msg.events).length !== 0">
+                            <v-btn icon>
+                                <v-icon>fa-calendar-alt</v-icon>
+                            </v-btn>
+                        </v-badge>
+                    </v-card-actions>
+                </v-card>
+                <v-spacer></v-spacer>
+            </v-row>
+
+            <!-- Message sortant -->
+            <v-row align="center" class="mb-3">
+                <v-spacer></v-spacer>
+                <v-card outlined>
+                    <v-card-text>Ceci est un message</v-card-text>
+                    <v-divider></v-divider>
+                </v-card>
+                <v-col class="col-auto">
+                    <v-avatar color="">
+                        <v-icon>fa-user</v-icon>
+                    </v-avatar>
+                </v-col>
+            </v-row>
         </v-card>
 
         <!-- Message text field -->
-        <v-row class="ml-1">
-            <v-text-field
-                outlined
-                clearable
-                hide-details
-                v-model="message">
+        <v-row class="pl-3 pr-3" id="textfield">
+                <v-text-field
+                    outlined
+                    clearable
+                    hide-details
+                    v-model="message">
 
-            </v-text-field>
-            <v-btn height="auto" class="m-0" v-on:click="sendMessage">
-                <v-icon>fa-paper-plane</v-icon>
-            </v-btn>
+                </v-text-field>
+                <v-btn height="auto" class="ml-1" v-on:click="sendMessage">
+                    <v-icon>fa-paper-plane</v-icon>
+                </v-btn>
         </v-row>
-    </div>
+    </v-card>
 </template>
 
 <script>
@@ -30,14 +72,16 @@
         props: ['salon_id'],
         data() {
             return {
-                message: ''
+                message: '',
+                messages: []
             }
         },
         created() {
             Echo.private('salon.'+this.salon_id)
                 .listen('MessageSended', (e) => {
                     //New messages
-                    console.log(e)
+                    console.log(e);
+                    this.messages.push(e);
                 });
         },
         methods: {
@@ -56,5 +100,26 @@
 </script>
 
 <style scoped>
+    #chat{
+        position: absolute;
+        top: 0;
+    }
 
+    #chatfield{
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 60px;
+
+        overflow-x: hidden;
+        overflow-y: scroll;
+    }
+
+    #textfield{
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
 </style>
